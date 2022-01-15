fa02cdd548605719e7eb4eb95f24788ea497f0eb
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2022-01-15 18:43:39 +0300

    Checked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 const regexp = /^\w+\@(\w+\.)+\w+$/;
+// Абсолютно верный РВ!
+// РВ для проверки электронной почты в элементе input с типом email на самом деле гораздо больше, чем данное РВ – на него вы можете посмотреть на этом сайте https://emailregex.com/ или на этом https://regexr.com/.
 
 function testStr(str) {
     let result = regexp.test(str);
